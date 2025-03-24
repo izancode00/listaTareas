dd7186c0bf10ffe81a41e8586b6509e8c19a093c
arior <arior@indra.es>
2025-03-24 11:49:26 +0100

    Se corrige un error en la distribución de carpetas

--- src/main/java/com/listaTareas/ListaTareasApplication.java
+++ src/main/java/com/listaTareas/ListaTareasApplication.java
@@ -1,4 +1,4 @@
-package com.example.listaTareas;
+package com.listaTareas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
