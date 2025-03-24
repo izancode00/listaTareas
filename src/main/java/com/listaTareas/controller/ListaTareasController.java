40c36ae136551b71d010a75a791b3c8f6e13ecfb
izan rosa <eizanrosa10@gmail.com>
2025-03-24 15:58:38 +0100

    subir

---
+++ src/main/java/com/listaTareas/controller/ListaTareasController.java
@@ -0,0 +1,82 @@
+package com.listaTareas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.listaTareas.model.Tarea;
+import com.listaTareas.service.TareaService;
+
+@Controller
+@RequestMapping("/lista")
+public class ListaTareasController {
+    private final TareaService tareasService;
+
+    public ListaTareasController(TareaService tareasService) {
+        this.tareasService = tareasService;
+    }
+
+    @GetMapping("")
+    public String mostrarTarea(Model model, @ModelAttribute("message") String mensaje){
+        //model.addAttribute("agenda", tareasService.contactoService.obtenerContacto());
+        model.addAttribute("message",mensaje);
+        return "lista";
+    }
+
+    @GetMapping("/nueva")
+    public String nuevoContactoForm(Model model){
+        model.addAttribute("tarea", new Tarea());
+        return "nuevaTarea";
+    }
+
+    @PostMapping("/guardar")
+    public String guardarContacto(@ModelAttribute Tarea tarea, RedirectAttributes redirectAttributes){
+        //contactoService.guardarContacto(contacto);
+        redirectAttributes.addFlashAttribute("guardar", "Tarea guardada con éxito");
+        return "redirect:/lista";
+    }
+
+    @PostMapping("/eliminar/{nombre}")
+    public String eliminarTarea(@PathVariable("nombre") String nombre, RedirectAttributes redirectAttributes){
+        boolean eliminado = tareasService.eliminarTarea(nombre);
+        if(eliminado){
+            redirectAttributes.addAttribute("message", "Tarea eliminada con éxito");
+        }else{
+            redirectAttributes.addFlashAttribute("message", "No se pudo eliminar la tarea");
+        }
+        return "redirect:/lista";
+    }
+
+    @GetMapping("/editar/{nombre}")
+    public String editarTarea(@PathVariable("nombre") String nombre, Model model){
+        Tarea tarea = tareasService.obtenerTareaPorNombre(nombre);
+        model.addAttribute("tarea", tarea);
+        return "editarTarea";
+    }
+
+    @PostMapping("/actualizar/{nombre}")
+    public String actualizarTarea(@PathVariable("nombre") String nombreOriginal, Tarea tareaActualizada, RedirectAttributes redirectAttributes){
+        tareasService.editarTarea(nombreOriginal);
+        redirectAttributes.addFlashAttribute("message", "Tarea actualizada con éxito");
+        return "redirect:/lista";
+    }
+
+    @PostMapping("/finalizada/{nombre}")
+    public String finalizarTarea(@PathVariable("nombre") String nombre, RedirectAttributes redirectAttributes){
+        tareasService.terminarTarea(nombre);
+        redirectAttributes.addFlashAttribute("message", "Tarea finalizada");
+        return "redirect:/lista";
+    }
+
+
+
+
+
+
+    
+}
--- src/main/java/com/listaTareas/service/TareaService.java
+++ src/main/java/com/listaTareas/service/TareaService.java
@@ -12,17 +12,41 @@ public class TareaService {
     private Map<String, Tarea> tareasTerminadas = new HashMap<>();
 
     // Método para añadir una tarea a tareasPendientes
-    public void añadirTarea(String id, Tarea tarea) {
-        tareasPendientes.put(id, tarea);
+    public void añadirTarea(String nombre, Tarea tarea) {
+        tareasPendientes.put(nombre, tarea);
     }
 
     // Método para mover una tarea de tareasPendientes a tareasTerminadas
-    public void terminarTarea(String id) {
+    public void terminarTarea(String nombre) {
+        Tarea tarea = tareasPendientes.remove(nombre);
+        if (tarea != null) {
+            tareasTerminadas.put(nombre, tarea);
+        }
+    }
+
+    //editar por nombre
+    public void editarTarea(String id) {
         Tarea tarea = tareasPendientes.remove(id);
         if (tarea != null) {
             tareasTerminadas.put(id, tarea);
         }
     }
+//obtener por nombre
+public Tarea obtenerTareaPorNombre(String nombre) {
+    return tareasPendientes.get(nombre);
+}
+//borrar
+public boolean eliminarTarea(String nombre){
+    Tarea tarea = tareasPendientes.get(nombre);
+    if(tarea != null){
+        tareasPendientes.remove(nombre);
+        return true;
+    }else if (tareasPendientes.get(nombre) != null){
+    tareasTerminadas.remove(nombre);
+    return true;
+    }else{
+        return false;
+    }
 
-    // Métodos adicionales para gestionar las tareas pueden ser añadidos aquí
+    }
 }
